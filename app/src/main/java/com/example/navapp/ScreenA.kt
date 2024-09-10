8233466aa6266816ff5c5343ddf5696480262b1d
Nicholos Nauth <77813014+nicholosnauth@users.noreply.github.com>
2024-09-10 14:35:12 -0400

    added button

--- app/src/main/java/com/example/navapp/ScreenA.kt
+++ app/src/main/java/com/example/navapp/ScreenA.kt
@@ -1,2 +1,17 @@
 package com.example.navapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ScreenA(){
+    Column {
+        Text(text = "Screen A")
+        Button(onClick = {}){
+            Text(text = "")
+        }
+    }
+}
