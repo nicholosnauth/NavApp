0fe4d92c970772a2b22334e15f8c887cdbaca618
Nicholos Nauth <77813014+nicholosnauth@users.noreply.github.com>
2024-09-10 15:23:25 -0400

    Button functional, navigates to another page

--- app/src/main/java/com/example/navapp/MainActivity.kt
+++ app/src/main/java/com/example/navapp/MainActivity.kt
@@ -4,22 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.navapp.ui.theme.NavAppTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
     class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
-
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "screen_A")  {
+                composable("screen_A") {
+                    ScreenA(navController)
+                }
+                composable("screen_B") {
+                    ScreenB()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/navapp/ScreenA.kt
+++ app/src/main/java/com/example/navapp/ScreenA.kt
@@ -1,17 +1,28 @@
 package com.example.navapp
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 
 
 @Composable
-fun ScreenA(){
-    Column {
+fun ScreenA(navController: NavController){
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         Text(text = "Screen A")
-        Button(onClick = {}){
-            Text(text = "")
+        Button(onClick = {
+            navController.navigate("screen_B")
+        }){
+            Text(text = "Go to Screen B")
         }
     }
 }
